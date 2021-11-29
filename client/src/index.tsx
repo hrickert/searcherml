dd991dde2fe81af66508fc0782020ac24ed9e9c9
Hernan Rickert <hernan@acronex.com>
2021-11-29 14:54:45 -0300

    se elimina dependencia nprogress

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import RootStore from './stores/RootStore';
-import { reaction } from 'mobx';
 import Main from './Main';
 
 const rootStore = new RootStore();
 (window as any).rootStore = rootStore;
-reaction(
-  () => {
-    return rootStore.uiStore.pendingProgress === 0;
-  },
-  (zeroReqs) => {
-    if (zeroReqs) {
-      NProgress.done();
-    } else {
-      NProgress.configure({ showSpinner: false });
-      NProgress.inc();
-    }
-  },
-  { fireImmediately: true }
-);
 
 ReactDOM.render(
   <React.StrictMode>
